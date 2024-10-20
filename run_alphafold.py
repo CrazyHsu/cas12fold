3af4a60110bebc0fb6cb35a0764d8782f2023290
Feng Xu <crazyhsu9527@gmail.com>
2024-10-21 01:52:29 +0800

    remove some files

--- run_alphafold.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File   : run_alphafold.py
-# @Author : Feng Xu @ crazyhsu9527@gmail.com
-# @Created: 2024-10-14
--- template_concatenation/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File   : __init__.py
-# @Author : Feng Xu @ crazyhsu9527@gmail.com
-# @Created: 2024-09-12
